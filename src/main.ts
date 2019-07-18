2d5a04bbdf9168ed0eb45d2124f3623b6d09fa67
yasmiyazaki <kizayami.yasu@gmail.com>
2019-07-18 12:59:14 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+
+import Auth from "@aws-amplify/auth";
+import API from "@aws-amplify/api";
+import PubSub from "@aws-amplify/pubsub";
+import amplify from "./aws-exports";
+Auth.configure(amplify);
+API.configure(amplify);
+PubSub.configure(amplify);
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
